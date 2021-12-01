b63e196fde89242eed973025b4873047fcd3611f
root <AhmedLaravel>
2021-12-02 00:11:28 +0200

    font awsome is not loasding without network

--- web_store/resources/views/website/layouts/header.blade.php
+++ web_store/resources/views/website/layouts/header.blade.php
@@ -7,12 +7,10 @@
     <link rel="icon" href="{{asset('website')}}/favicon.ico" type="image/x-icon">
 
     <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
-    {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.6.9/sweetalert2.min.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.6.9/sweetalert2.min.js"></script> --}}
-    <!-- Bootstrap Core Css -->
+   
     <link href="{{asset('website')}}/plugins/bootstrap/css/bootstrap.css" rel="stylesheet">
+    <link href="{{asset('website')}}/plugins/materialicsone.css" rel="stylesheet" type="text/css">
+    <link href="{{asset('website')}}/plugins/materialicons2.css" rel="stylesheet" type="text/css">
     <!-- Waves Effect Css -->
     
     {{-- Sweet alert --}}
--- web_store/resources/views/website/reset_password.blade.php
+++ web_store/resources/views/website/reset_password.blade.php
@@ -9,8 +9,8 @@
     <link rel="icon" href="{{ asset('website') }}/favicon.ico" type="image/x-icon">
 
     <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
+    <link href="{{asset('website')}}/plugins/materialicsone.css" rel="stylesheet" type="text/css">
+    <link href="{{asset('website')}}/plugins/materialicons2.css" rel="stylesheet" type="text/css">
 
     <!-- Bootstrap Core Css -->
     <link href="{{ asset('website') }}/plugins/bootstrap/css/bootstrap.css" rel="stylesheet">
